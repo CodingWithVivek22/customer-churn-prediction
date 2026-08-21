75e413d4057b705532f25089984cd800e2e37ba0
vivek <vivekambaliya763@gmail.com>
2026-08-21 15:52:28 +0530

    Connect frontend to deployed API

--- frontend/script.js
+++ frontend/script.js
@@ -81,7 +81,7 @@ form.addEventListener("submit", async function (event) {
 
     try {
 
-        const response = await fetch("http://127.0.0.1:8000/predict", {
+        const response = await fetch("https://customer-churn-prediction-exkn.onrender.com/predict", {
 
             method: "POST",
 
