2041cf4cf3d20335df2f8bb00a12b57460d46f66
vivek <vivekambaliya763@gmail.com>
2026-08-21 15:16:18 +0530

    Initial commit

---
+++ deployement/app.py
@@ -0,0 +1,140 @@
+from typing import Literal
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+import joblib
+import json
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://127.0.0.1:5500"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+scaler = joblib.load("../model/scaler.pkl")
+
+with open("../model/feature_columns.json", "r") as f:
+    feature_columns = json.load(f)
+
+import torch
+import torch.nn as nn
+
+class ANN(nn.Module):
+
+    def __init__(self, input_size):
+        super().__init__()
+
+        self.model = nn.Sequential(
+
+            nn.Linear(input_size, 64),
+            nn.ReLU(),
+
+            nn.Linear(64, 32),
+            nn.ReLU(),
+
+            nn.Linear(32, 1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+
+        return self.model(x)
+
+model = ANN(len(feature_columns))
+
+model.load_state_dict(
+    torch.load("../model/ann_model.pth", map_location="cpu")
+)
+
+model.eval()
+
+
+@app.get("/")
+def home():
+    return {"message" : "Customer Churn API is Running"}
+
+
+class Customer(BaseModel):
+
+    gender: int
+    SeniorCitizen: int
+    Partner: int
+    Dependents: int
+    tenure: float
+    PhoneService: int
+    MultipleLines: int
+    OnlineSecurity: int
+    OnlineBackup: int
+    DeviceProtection: int
+    TechSupport: int
+    StreamingTV: int
+    StreamingMovies: int
+    Contract: Literal[
+        "Month-to-month",
+        "One year",
+        "Two year"
+    ]
+    PaperlessBilling: int
+
+    MonthlyCharges: float
+    TotalCharges: float
+
+    InternetService: Literal[
+
+        "DSL",
+        "Fiber optic",
+        "No"
+    ]
+
+    PaymentMethod: Literal[
+
+        "Bank transfer (automatic)",
+        "Credit card (automatic)",
+        "Electronic check",
+        "Mailed check"
+    ]
+
+@app.post("/predict")
+def predict(customer: Customer):
+
+    import pandas as pd
+
+    data = customer.model_dump()
+    df = pd.DataFrame([data])
+
+    df = pd.get_dummies(
+        df, 
+        columns=["InternetService", "PaymentMethod", "Contract"]
+    )
+
+    df = df.reindex(
+        columns = feature_columns,
+        fill_value = 0
+    )
+
+    scaled_data = scaler.transform(df)
+
+    X = torch.FloatTensor(scaled_data)
+
+    with torch.no_grad():
+        output = model(X)
+
+    prediction = (output > 0.5).float()
+
+    return {
+
+        "churn_probability" : output.item(),
+        "prediction" : int(prediction.item())
+    }
+    
+    
+
+    
---
+++ frontend/script.js
@@ -0,0 +1,112 @@
+const form = document.getElementById("predictionForm");
+
+form.addEventListener("submit", async function (event) {
+
+    event.preventDefault();
+
+    console.log("BUTTON CLICKED");
+
+    const customerData = {
+
+        gender: Number(document.getElementById("gender").value),
+
+        SeniorCitizen: Number(
+            document.getElementById("SeniorCitizen").value
+        ),
+
+        Partner: Number(
+            document.getElementById("Partner").value
+        ),
+
+        Dependents: Number(
+            document.getElementById("Dependents").value
+        ),
+
+        tenure: Number(
+            document.getElementById("tenure").value
+        ),
+
+        PhoneService: Number(
+            document.getElementById("PhoneService").value
+        ),
+
+        MultipleLines: Number(
+            document.getElementById("MultipleLines").value
+        ),
+
+        OnlineSecurity: Number(
+            document.getElementById("OnlineSecurity").value
+        ),
+
+        OnlineBackup: Number(
+            document.getElementById("OnlineBackup").value
+        ),
+
+        DeviceProtection: Number(
+            document.getElementById("DeviceProtection").value
+        ),
+
+        TechSupport: Number(
+            document.getElementById("TechSupport").value
+        ),
+
+        StreamingTV: Number(
+            document.getElementById("StreamingTV").value
+        ),
+
+        StreamingMovies: Number(
+            document.getElementById("StreamingMovies").value
+        ),
+
+        Contract: document.getElementById("Contract").value,
+
+        PaperlessBilling: Number(
+            document.getElementById("PaperlessBilling").value
+        ),
+
+        MonthlyCharges: Number(
+            document.getElementById("MonthlyCharges").value
+        ),
+
+        TotalCharges: Number(
+            document.getElementById("TotalCharges").value
+        ),
+
+        InternetService:
+            document.getElementById("InternetService").value,
+
+        PaymentMethod:
+            document.getElementById("PaymentMethod").value
+    };
+
+    try {
+
+        const response = await fetch("http://127.0.0.1:8000/predict", {
+
+            method: "POST",
+
+            headers: {
+                "Content-Type": "application/json"
+            },
+
+            body: JSON.stringify(customerData)
+
+        });
+
+        const result = await response.json();
+
+        console.log(result);
+
+        document.getElementById("result").innerHTML = `
+    <h2>Prediction Result</h2>
+    <p>Churn Probability: ${(result.churn_probability * 100).toFixed(2)}%</p>
+    <p>Prediction: ${result.prediction === 1 ? "Churn" : "No Churn"}</p>
+`;
+
+    } catch (error) {
+
+        console.error("Error:", error);
+
+    }
+
+});
